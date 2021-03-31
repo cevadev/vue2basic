bcc78a717566e02ca4758842843137fa4bfbcb23
Carlos Eduardo Villanueva Altuna <carlos.villanueva.altuna@gmail.com>
2021-03-31 18:34:44 -0300

    Curso basico Vue 2 - Rendering declarativo

---
+++ clase2-computed-properties/app.js
@@ -0,0 +1,40 @@
+new Vue({
+  /**Propiedades de la instancia Vue: el, data(), methods, computed */
+  el: "#app",
+  data() {
+    return {
+      name: "Bitcoin",
+      symbol: "BTC",
+      img: "https://cryptologos.cc/logos/bitcoin-btc-logo.png",
+      changePercent: 0,
+      price: 8400,
+      showPrices: false,
+      pricesWithDays: [
+        { day: "Lunes", value: 8400 },
+        { day: "Martes", value: 7900 },
+        { day: "Miercoles", value: 8200 },
+        { day: "Jueves", value: 9000 },
+        { day: "Viernes", value: 9400 },
+        { day: "Sabado", value: 10000 },
+        { day: "Domingo", value: 10200 },
+      ],
+    };
+  },
+  methods: {
+    toggleShowPrices() {
+      this.showPrices = !this.showPrices;
+    },
+  },
+  //definicion de propiedades computadas
+  computed: {
+    title() {
+      return `${this.name}-${this.symbol}`;
+    },
+  },
+  watch: {
+    //watcher para saber cada que cambia showPrices
+    showPrices(newVal, oldVal) {
+      console.info(newVal, oldVal);
+    },
+  },
+});
